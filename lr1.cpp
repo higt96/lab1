ca3aa41bb349c0ea35cb8daddae08eb4c77cd648
higt96 <higt96@gmail.com>
2017-10-17 06:25:58 +0300

    2
    Добавлено зчитування з файла
    В алгоритм злиттям вставлено алгоритм вставками
    Добавлено обрахунок часу

--- lr1.cpp
+++ lr1.cpp
@@ -1,17 +1,48 @@
 // lr1.cpp: определяет точку входа для консольного приложения.
 //
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 #include <conio.h>
 #include "stdafx.h"
+#include <windows.h>
+#include <algorithm>
 using namespace std;
-#define maxn 100
-
-
-int a[maxn];
 int mas[];
-int n;
-int* foo = mas;
 
+void getn(int& x) {
+	ifstream fin("min.txt");
+	x=0;
+	while (!fin.eof())
+	{	x++;	}
+	fin.close();
+
+}
+void masin(int* mas)
+{
+	ifstream fin("min.txt");
+	
+	int x=0;
+	while (!fin.eof())
+	{
+		fin >> mas[x];
+
+			x++;
+	}
+	fin.close();
+		
+	
+}
+
+void masout(int* mas,int n)
+{
+	ofstream fout("mout.txt"); 
+	for (int i = 0; i < n; i++) {
+		fout << mas[i];
+	}
+	fout.close(); 
+	
+}
 
 void insertion_sort(int l,int r, int* arr)
 {
@@ -30,7 +61,7 @@ void insertion_sort(int l,int r, int* arr)
 	}
 }
 
-	void merge(int l, int r) {
+	void merge(int l, int r,int* mas) {
 		if (r == l)
 			return;
 		if (r - l <=10) {
@@ -39,67 +70,48 @@ void insertion_sort(int l,int r, int* arr)
 			return;
 		}
 		int m = (r + l) / 2;
-		merge(l, m);
-		merge(m + 1, r);
-		int buf[maxn];
+		merge(l, m, mas);
+		merge(m + 1, r, mas);
+		int buf[n];
 		int xl = l;
 		int xr = m + 1;
 		int cur = 0;
 		while (r - l + 1 != cur) {
 			if (xl > m)
-				buf[cur++] = a[xr++];
+				buf[cur++] = mas[xr++];
 			else if (xr > r)
-				buf[cur++] = a[xl++];
-			else if (a[xl] > a[xr])
-				buf[cur++] = a[xr++];
-			else buf[cur++] = a[xl++];
+				buf[cur++] = mas[xl++];
+			else if (mas[xl] > mas[xr])
+				buf[cur++] = mas[xr++];
+			else buf[cur++] = mas[xl++];
 
 		}
 		for (int i = 0; i < cur; i++)
-			a[i + l] = buf[i];
+			mas[i + l] = buf[i];
 
 }
-	void merge1(int l, int r) {
-		if (r == l)
-			return;
-		if (r - l == 1) {
-			if (a[r] < a[l])
-				swap(a[r], a[l]);
-			return;
-		}
-		int m = (r + l) / 2;
-		merge(l, m);
-		merge(m + 1, r);
-		int buf[maxn];
-		int xl = l;
-		int xr = m + 1;
-		int cur = 0;
-		while (r - l + 1 != cur) {
-			if (xl > m)
-				buf[cur++] = a[xr++];
-			else if (xr > r)
-				buf[cur++] = a[xl++];
-			else if (a[xl] > a[xr])
-				buf[cur++] = a[xr++];
-			else buf[cur++] = a[xl++];
-
-		}
-		for (int i = 0; i < cur; i++)
-			a[i + l] = buf[i];
-
-	}
+	
 	int main() {
-		cin >> n;
+				
+		int n = 0;
+		getn(n);
+		int mas[n];
+		masin(mas);
 
-		for (int i = 0; i < n; i++)
-			cin >> a[i];
+		DWORD start = GetTickCount();
+		for (int i = 0; i < 1000; i++)
 
-		merge(0, n - 1);
+			merge(0, n - 1, mas);
 
-		for (int i = 0; i < n; i++)
-			cout << a[i] << " ";
+		DWORD end = GetTickCount();
+		cout << (unsigned int)(end - start) << endl;
+		cin.get();
+		masout(mas,n);
+		return 0;
+		
+		
 
-		getch();
+
 		return 0;
 	}
 
